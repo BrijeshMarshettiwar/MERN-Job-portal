5a113333b7565095837d2cce03f3acd24494255e
Brijesh Marshettiwar <123184136+BrijeshMarshettiwar@users.noreply.github.com>
2024-03-31 18:31:22 +0530

    Update index.js

--- job-portal-server/index.js
+++ job-portal-server/index.js
@@ -14,7 +14,7 @@ app.use(cors())
 // password: GoSu0j1SMYMtf0FF
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@job-portal-demo.xc6j6sz.mongodb.net/?retryWrites=true&w=majority&appName=job-portal-demo`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -60,7 +60,17 @@ async function run() {
     // get jobs by email
     app.get("/myJobs/:email", async(req, res) =>{
       // console.log(req.params.email)
-      const jobs = await jobsCollections.find({postedBy :""})
+      const jobs = await jobsCollections.find({postedBy : req.params.email}).toArray();
+      res.send(jobs)
+    })
+
+
+    // delete a job
+    app.delete("/job/:id", async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const result = await jobsCollections.deleteOne(filter);
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
