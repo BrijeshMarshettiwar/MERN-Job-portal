ec12588c4b432a4d23f7b04dfc7c64b5dec7cc05
Brijesh Marshettiwar <123184136+BrijeshMarshettiwar@users.noreply.github.com>
2024-04-04 14:51:41 +0530

    Update index.js

--- job-portal-server/index.js
+++ job-portal-server/index.js
@@ -82,6 +82,22 @@ async function run() {
       res.send(result)
     })
 
+    // update a job
+    app.patch("/update-jb/:id", async(req, res) => {
+      const id = req.params.id;
+      const jobData = req.body;
+      const filter = {_id: new ObjectId(id)};
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          ...jobData
+        },
+      };
+
+      const result = await jobsCollections.updateOne(filter, updateDoc, options);
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
